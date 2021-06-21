2db73be67660c7f87abcc7f37921c83656b30794
Fer250-999 <fero250999upcdr@gmail.com>
2021-06-20 22:18:21 -0500

    Creacion: Controlador Administrador y Vista Home, Layout y Layout Parcial

---
+++ AutoShop/Controllers/AdminstradorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoShop.Controllers
+{
+    public class AdminstradorController : Controller
+    {
+        // GET: Adminstrador
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
