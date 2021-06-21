86082fa3c502b2563e6c229da6ca07f5f77f07fe
JuanButronC <jon4589@hotmail.com>
2021-06-20 22:40:29 -0500

    Se crea controlador Pago

---
+++ AutoShop/Controllers/PagoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutoShop.Models;
+
+namespace AutoShop.Controllers
+{
+    [Authorize]
+    public class PagoController : Controller
+    {
+        private contextAutoShop db = new contextAutoShop();
+        // GET: Pago
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
