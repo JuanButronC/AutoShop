d4c972487ef9fb1e0e5769a0c564f1718567619c
Fer250-999 <fero250999upcdr@gmail.com>
2021-06-20 22:22:31 -0500

    Adminstrador:Diseño y funcionamiento Home

--- AutoShop/Controllers/AdminstradorController.cs
+++ AutoShop/Controllers/AdminstradorController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AutoShop.Models;
+using System.Data.Entity;
 
 namespace AutoShop.Controllers
 {
     public class AdminstradorController : Controller
     {
-        // GET: Adminstrador
+        private contextAutoShop db = new contextAutoShop();
+
+        // GET: Administrador
         public ActionResult Index()
         {
+            var administradores = db.Empleado.Include(e => e.Roles);
+            ViewBag.total = administradores.Where(e => e.rol_fk != 3).Count();
+            ViewBag.totalCompras = administradores.Where(e => e.rol_fk == 1).Count();
+            ViewBag.totalAlmacen = administradores.Where(e => e.rol_fk == 2).Count();
+            ViewBag.listaCompras = administradores.Where(e => e.rol_fk == 1).ToList();
+            ViewBag.listaAlmacen = administradores.Where(e => e.rol_fk == 2).ToList();
+
             return View();
         }
     }
