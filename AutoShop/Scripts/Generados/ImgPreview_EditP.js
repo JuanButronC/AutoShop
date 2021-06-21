67721648dd01e1b900d848b04352ca5f84ed7c87
JuanButronC <jon4589@hotmail.com>
2021-06-20 23:06:36 -0500

    Se añade vista Home para Rol Almacen y JS para previsualizar imagenes en CRUD Paqueteria

---
+++ AutoShop/Scripts/Generados/ImgPreview_EditP.js
@@ -0,0 +1,15 @@
+﻿//Para Edit Paquetería
+
+const $inputImageEdit = document.querySelector("#inputImageEditP");
+const $imagenPrevisualizacionEdit = document.querySelector("#ImageEditP");
+
+$inputImageEdit.addEventListener("change", () => {
+    const archivos = $inputImageEdit.files;
+    if (!archivos || !archivos.length) {
+        $imagenPrevisualizacionEdit.src = "";
+        return;
+    }
+    const primerArchivo = archivos[0];
+    const objectURL = URL.createObjectURL(primerArchivo);
+    $imagenPrevisualizacionEdit.src = objectURL;
+});
