44eb094a9850ca1f06b1ca52d981311bd5e539fc
DiazAndres <heladio476@gmail.com>
2021-06-20 23:52:58 -0500

    Modificación controlador producto, se añaden métodos, vistas y js para productos

--- AutoShop/Controllers/productoController.cs
+++ AutoShop/Controllers/productoController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -48,18 +50,91 @@ namespace AutoShop.Controllers
         // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "id,nombre,id_categoria,descripcion,imagen,precio,descuento,cantidad")] Producto producto)
+        public ActionResult Create([Bind(Include = "id,nombre,id_categoria,descripcion,descuento,precio,cantidad")] Producto producto, HttpPostedFileBase imagen)
         {
+            if (imagen != null && imagen.ContentLength > 0)
+            {
+                using (var reader = new System.IO.BinaryReader(imagen.InputStream))
+                {
+                    producto.imagen = reader.ReadBytes(imagen.ContentLength);
+                }
+            }
             if (ModelState.IsValid)
             {
-                db.Producto.Add(producto);
-                db.SaveChanges();
+                Producto prod = new Producto();
+                prod.id = producto.id;
+                prod.nombre = producto.nombre;
+                prod.id_categoria = producto.id_categoria;
+                prod.descripcion = producto.descripcion;
+                prod.imagen = producto.imagen;
+                prod.precio = producto.precio;
+                prod.descuento = Descuento(producto.descuento);
+                prod.cantidad = producto.cantidad;
+
+                db.Producto.Add(prod);
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbEntityValidationException dbEx)
+                {
+                    foreach (var validationErrors in dbEx.EntityValidationErrors)
+                    {
+                        foreach (var validationError in validationErrors.ValidationErrors)
+                        {
+                            Trace.TraceInformation("Property: {0} Error: {1}",
+                                validationError.PropertyName,
+                                validationError.ErrorMessage);
+                        }
+                    }
+                }
+
                 return RedirectToAction("Index");
             }
 
             ViewBag.id_categoria = new SelectList(db.Categoria, "id", "nombre", producto.id_categoria);
             return View(producto);
         }
+
+        public decimal Descuento(decimal? desc)
+        {
+            decimal descuento = 0;
+            switch (desc)
+            {
+                case 10:
+                    descuento = 10;
+                    break;
+                case 20:
+                    descuento = 20;
+                    break;
+                case 30:
+                    descuento = 30;
+                    break;
+                case 40:
+                    descuento = 40;
+                    break;
+                case 50:
+                    descuento = 50;
+                    break;
+                case 60:
+                    descuento = 60;
+                    break;
+                case 70:
+                    descuento = 70;
+                    break;
+                case 80:
+                    descuento = 80;
+                    break;
+                case 90:
+                    descuento = 90;
+                    break;
+                case 100:
+                    descuento = 100;
+                    break;
+            }
+            return descuento;
+        }
+
 
         // GET: producto/Edit/5
         public ActionResult Edit(int? id)
@@ -82,11 +157,21 @@ namespace AutoShop.Controllers
         // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "id,nombre,id_categoria,descripcion,imagen,precio,descuento,cantidad")] Producto producto)
+        public ActionResult Edit([Bind(Include = "id,nombre,id_categoria,descuento,descripcion,precio,cantidad")] Producto producto)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(producto).State = EntityState.Modified;
+                int id = producto.id;
+                var prod = db.Producto.Find(id);
+                decimal precio_ant = prod.precio;
+                decimal precio_act = producto.precio;
+                //db.Entry(producto).State = EntityState.Modified;
+                prod.nombre = producto.nombre;
+                prod.descripcion = producto.descripcion;
+                prod.descuento = producto.descuento;
+                prod.precio = producto.precio;
+                prod.cantidad = producto.cantidad;
+                prod.id_categoria = producto.id_categoria;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
@@ -128,5 +213,40 @@ namespace AutoShop.Controllers
             }
             base.Dispose(disposing);
         }
+
+        //metodo para obtener imagen
+        public ActionResult getImg(int id)
+        {
+            Producto imgPaq = (from i in db.Producto
+                               where i.id == id
+                               select i).ToList().FirstOrDefault();
+
+            var fileToRetrieve = imgPaq.imagen;
+            return File(fileToRetrieve, "image/jpeg");
+        }
+
+        public ActionResult getProductos()
+        {
+            var producto = db.Producto.OrderByDescending(e => e.id)
+                .Include(p => p.Categoria).Take(5).ToList();
+            return View(producto);
+        }
+
+        public ActionResult getExistencias()
+        {
+            var producto = db.Producto.OrderBy(e => e.cantidad)
+                .Include(p => p.Categoria).Take(5).ToList();
+            return View(producto);
+        }
+
+        public ActionResult getProdMayor()
+        {
+            var producto = db.Producto.OrderByDescending(e => e.cantidad)
+                .Include(p => p.Categoria).Take(1).ToList();
+            return View(producto);
+        }
+
+
+
     }
 }
---
+++ AutoShop/Scripts/Generados/Productos/Productos.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        url: "producto/getProductos",
+        success: function (result) {
+            $("#tablaProductos").html(result);
+        }
+    });
+
+    $.ajax({
+        url: "producto/getExistencias",
+        success: function (result) {
+            $("#tablaExistencias").html(result);
+        }
+    });
+
+    $.ajax({
+        url: "producto/getProdMayor",
+        success: function (result) {
+            $("#producto").html(result);
+        }
+    });
+
+    const $seleccionArchivos = document.querySelector("#seleccionArchivos"),
+        $imagenPrevisualizacion = document.querySelector("#imagenPrevisualizacion");
+
+        $seleccionArchivos.addEventListener("change", () => {
+        const archivos = $seleccionArchivos.files;
+        if (!archivos || !archivos.length) {
+            $imagenPrevisualizacion.src = "";
+            return;
+        }
+        const primerArchivo = archivos[0];
+        const objectURL = URL.createObjectURL(primerArchivo);
+        $imagenPrevisualizacion.src = objectURL;
+    });
+
+});
