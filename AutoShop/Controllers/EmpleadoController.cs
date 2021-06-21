540a99772d64d8afb92140949f2cd79360925ba0
Fer250-999 <fero250999upcdr@gmail.com>
2021-06-20 22:25:05 -0500

    Creacion:CRUD Empleados

---
+++ AutoShop/Controllers/EmpleadoController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AutoShop.Models;
+
+namespace AutoShop.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        private contextAutoShop db = new contextAutoShop();
+
+        // GET: Empleado
+        public ActionResult Index()
+        {
+            var empleado = db.Empleado.Include(e => e.ImagenesEmpleado).Include(e => e.Roles);
+            return View(empleado.ToList());
+        }
+
+        // GET: Empleado/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleado empleado = db.Empleado.Find(id);
+            if (empleado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleado);
+        }
+
+        // GET: Empleado/Create
+        public ActionResult Create()
+        {
+            ViewBag.imagen_fk = new SelectList(db.ImagenesEmpleado, "id", "id");
+            ViewBag.rol_fk = new SelectList(db.Roles, "id", "nombre");
+            return View();
+        }
+
+        // POST: Empleado/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombre,contrasenia,correo,rol_fk,imagen_fk")] Empleado empleado)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Empleado.Add(empleado);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.imagen_fk = new SelectList(db.ImagenesEmpleado, "id", "id", empleado.imagen_fk);
+            ViewBag.rol_fk = new SelectList(db.Roles, "id", "nombre", empleado.rol_fk);
+            return View(empleado);
+        }
+
+        // GET: Empleado/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleado empleado = db.Empleado.Find(id);
+            if (empleado == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.imagen_fk = new SelectList(db.ImagenesEmpleado, "id", "id", empleado.imagen_fk);
+            ViewBag.rol_fk = new SelectList(db.Roles, "id", "nombre", empleado.rol_fk);
+            return View(empleado);
+        }
+
+        // POST: Empleado/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombre,contrasenia,correo,rol_fk,imagen_fk")] Empleado empleado)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(empleado).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.imagen_fk = new SelectList(db.ImagenesEmpleado, "id", "id", empleado.imagen_fk);
+            ViewBag.rol_fk = new SelectList(db.Roles, "id", "nombre", empleado.rol_fk);
+            return View(empleado);
+        }
+
+        // GET: Empleado/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleado empleado = db.Empleado.Find(id);
+            if (empleado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleado);
+        }
+
+        // POST: Empleado/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Empleado empleado = db.Empleado.Find(id);
+            db.Empleado.Remove(empleado);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
